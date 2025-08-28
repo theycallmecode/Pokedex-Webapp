821cf3ad61b2679b6fb1e63805ba0bf9583c8b60
Prism <ershubhcsofficial@gmail.com>
2025-08-29 00:50:00 +0530

    added custom colors for pkmn types

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const poke_container = document.getElementById("poke-container");
 const pokemon_count = 1010;
+
+const colors = {
+  fire: "#e03a3a",
+  grass: "#50C878",
+  electric: "#fad343",
+  water: "#1E90FF",
+  ground: "#735139",
+  rock: "#63594f",
+  fairy: "#EE99AC",
+  poison: "#b34fb3",
+  bug: "#A8B820",
+  dragon: "#fc883a",
+  psychic: "#882eff",
+  flying: "#87CEEB",
+  fighting: "#bf5858",
+  normal: "#D2B48C",
+  ghost: "#7B62A3",
+  dark: "#414063",
+  steel: "#808080",
+  ice: "#98D8D8",
+};
